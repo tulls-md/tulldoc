374ff7438e3980cbfd93113d1fbb3f2df07fec7a
Alexdem <alexdem@tulls.ru>
2026-06-17 00:04:13 +0300

    Add ccs modules scoped names

--- lib-code/vite.config.ts
+++ lib-code/vite.config.ts
@@ -1,10 +1,26 @@
+import { basename } from "node:path";
+import { createHash } from "node:crypto";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { nodeExternals } from "rollup-plugin-node-externals";
 import preserveDirectives from "rollup-preserve-directives";
 
+function generateScopedName(local: string, filename: string) {
+  const moduleName = basename(filename).replace(/\.module\.css$/, "");
+  const hash = createHash("sha256")
+    .update(`${filename}:${local}`)
+    .digest("base64url")
+    .slice(0, 5);
+  return `tulldoc-code-${moduleName}__${local}_${hash}`;
+}
+
 export default defineConfig({
+  css: {
+    modules: {
+      generateScopedName,
+    },
+  },
   plugins: [
     { ...nodeExternals(), enforce: "pre" },
     react(),
